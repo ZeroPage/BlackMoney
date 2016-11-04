430f19b19745fb8a1ade451a2afada39c11b7364
seongjoon <hs4393@gmail.com>
2016-11-05 06:08:28 +0900

    fail code
    do it again

---
+++ BlackMoney/app/src/main/java/com/example/zpadmin/blackmoney/payBackActivity.java
@@ -0,0 +1,107 @@
+package com.example.zpadmin.blackmoney;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class payBackActivity extends AppCompatActivity {
+    private ListView mListView = null;
+    private ListViewAdapter mAdapter = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payback);
+
+        mListView = (ListView) findViewById(R.id.mList);
+
+        mAdapter = new ListViewAdapter(this);
+        mListView.setAdapter(mAdapter);
+        //mAdapter.addItem();
+    }
+    private class ViewHolder {
+        public TextView name;
+
+        public TextView money;
+    }
+    class tmp{
+        public String name;
+        public String money;
+    }
+    private class ListViewAdapter extends BaseAdapter {
+        private Context mContext = null;
+        private ArrayList<tmp> mListData = new ArrayList<tmp>();
+
+        public ListViewAdapter(Context mContext) {
+            super();
+            this.mContext = mContext;
+        }
+
+        @Override
+        public int getCount() {
+            return mListData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mListData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.payback_listview_item, null);
+
+                holder.name = (TextView) convertView.findViewById(R.id.name);
+                holder.money = (TextView) convertView.findViewById(R.id.money);
+
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            tmp mData = mListData.get(position);
+
+
+            holder.name.setText(mData.name);
+            holder.money.setText(mData.money);
+
+            return convertView;
+        }
+        public void addItem( String name, String money){
+            tmp addInfo = null;
+            addInfo = new tmp();
+            addInfo.name = name;
+            addInfo.money = money;
+
+            mListData.add(addInfo);
+        }
+
+        public void remove(int position){
+            mListData.remove(position);
+            dataChange();
+        }
+
+        public void dataChange(){
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+}
